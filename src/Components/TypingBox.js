10f9dd258ba1e1e0dab80cb59ee9a2b05325b3b9
Jerico <jericodcosta95375788@gmail.com>
2023-10-20 18:54:37 +0530

    First commit

--- src/Components/TypingBox.js
+++ src/Components/TypingBox.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React,{useState} from "react";
 
 const para = `generally throw season needle who powder pretty burst accident cloud snow cage tool ahead frame planned feet our account purple from music five writer green mass biggest paragraph bark graph soon sudden range society drive victory die tightly plain raise college rod hurt remain cream raw leg mirror guard best`;
 
 const TypingBox = () => {
-    
+    const [time,setTime] = useState("15");
   return (
     <div className="typing-box">
       <div className="timer-container">
-        <div className="time">15</div>
+        <div className="time">{time}</div>
         <div className="time-option">
-          <button>15s</button>
-          <button>30s</button>
-          <button>60s</button>
+          <button onClick={()=>setTime("15")}>15s</button>
+          <button onClick={()=>setTime("30")}>30s</button>
+          <button onClick={()=>setTime("60")}>60s</button>
         </div>
       </div>
       <div className="text-container">
